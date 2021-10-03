9c83af6fcab8c5479b0fc6f46cf8faf23028cee2
masadaruma <masadaruma1225@gmail.com>
2021-10-03 22:07:22 +0900

    点滅まで完成

--- bunkasai_timer/bunkasai_timer.cpp
+++ bunkasai_timer/bunkasai_timer.cpp
@@ -11,82 +11,109 @@
 using namespace std;
 using namespace cv;
 
-#define FRAME_WIDTH 1920
+#define FRAME_WIDTH 1920	//画像のサイズ
 #define FRAME_HEIGHT 1200
-#define CAN_CANT_X 200
-#define CAN_CANT_Y 300
+//#define FRAME_HEIGHT 1080
+#define CAN_CANT_X 0		//入室可不可の座標
+#define CAN_CANT_Y 100
+#define MAX_NINN 25
 
-Mat people(int people_val);
+Mat people(int people_val, Mat img);
 Mat ninzu;
 Mat time_output(Mat img);
 Mat enter(Mat img);
-void PinP_point(const cv::Mat& srcImg, const cv::Mat& smallImg, const cv::Point2f p0, const cv::Point2f p1);
-bool out_flag=true;
+Mat PinP_tr(const cv::Mat& srcImg, const cv::Mat& smallImg, const int tx, const int ty);
+Mat Overview(Mat img);
+bool out_flag = false, time_flag;
+int old_sec;
 struct tm tm;
 Mat cant = imread("cant_enter.jpg"), can = imread("can_enter.jpg");
+Mat ninn = imread("ninn.jpg");
+Mat genzai = imread("ninzu.jpg");
 
 int main() {
 	int people_val;
 	ninzu=imread("ninzu.png");
 	namedWindow("show", WINDOW_AUTOSIZE|WINDOW_FREERATIO);
-	Mat img(Size(FRAME_WIDTH,FRAME_HEIGHT), CV_8UC3,Scalar(0,0,0));//最終的に表示する画像データ
-	img=people(10);
-	img=time_output(img);
-	img=enter(img);
-	imshow("show",img );
-	while (waitKey(10) != 113) {
+	while (1) {
+		Mat img(Size(FRAME_WIDTH, FRAME_HEIGHT), CV_8UC3, Scalar(255, 255, 255));//最終的に表示する画像データ
+		img = Overview(img);
+		img = people(10, img);
+		img = time_output(img);
+		img = enter(img);
+		imshow("show", img);
+		if(waitKey(1００)==113)break;
 	}
-	return 0;
 }
 
-Mat people(int people_val) {
-	Mat img = imread("ninzu.jpg");
-	putText(img, to_string(people_val), Point(FRAME_WIDTH/4, FRAME_HEIGHT/1.25),FONT_HERSHEY_TRIPLEX,30, Scalar(0,0 , 255),50);
+Mat Overview(Mat img) {
+	img=PinP_tr(img, genzai, FRAME_WIDTH/4, 0);//現在の人数
+	img=PinP_tr(img, ninn, FRAME_WIDTH-FRAME_WIDTH / 7, FRAME_HEIGHT-FRAME_HEIGHT / 4);//人
 	return img;
 }
 
-Mat time_output(Mat img) {
+Mat people(int people_val,Mat img) {//人数を出力
+	putText(img, to_string(people_val), Point(FRAME_WIDTH/4, FRAME_HEIGHT/1.5),FONT_HERSHEY_TRIPLEX,20, Scalar(0,0 , 255),50);
+	putText(img, "/"+to_string(MAX_NINN), Point(FRAME_WIDTH / 1.7, FRAME_HEIGHT / 1.3), FONT_HERSHEY_TRIPLEX, 10, Scalar(0, 0, 0), 30);
+	return img;
+}
+
+Mat time_output(Mat img) {//時間を出力
+	String time_st;
 	time_t t = time(NULL);
 	localtime_s(&tm,&t);
-	cout << tm.tm_hour << ":" << tm.tm_min << endl;
+	int sec = tm.tm_sec;
+	if (sec != old_sec) {
+		if (time_flag == true) {
+			time_flag = false;
+			cout << "be false" << endl;
+		}
+		else if (time_flag == false) {
+			time_flag = true;
+			cout << "be true" << endl;
+		}
+	}
+
+	old_sec = sec;
+	cout << tm.tm_hour << ":" << tm.tm_min << ":"<<sec<<endl;
 	string hour= to_string(tm.tm_hour),min= to_string(tm.tm_min);
+
 	if (tm.tm_hour <= 10)hour.insert(0, "0");
 	if (tm.tm_min <= 10)min.insert(0, "0");
-	String time_st = hour + ":" + min;
-	putText(img, time_st, Point(1300, 150), FONT_HERSHEY_TRIPLEX, 6, Scalar(0, 0, 0), 8);
+	if (time_flag ==true) {
+		time_st = hour + ":" + min;
+		putText(img, time_st, Point(FRAME_WIDTH * 0.67, FRAME_HEIGHT * 0.125), FONT_HERSHEY_TRIPLEX, 6, Scalar(0, 0, 0), 8);
+		cout << ":"<<endl;
+	}
+	else if (time_flag ==false) {
+		putText(img, hour, Point(FRAME_WIDTH * 0.67, FRAME_HEIGHT * 0.125), FONT_HERSHEY_TRIPLEX, 6, Scalar(0, 0, 0), 8);
+		putText(img, min, Point(FRAME_WIDTH * 0.67+306, FRAME_HEIGHT * 0.125), FONT_HERSHEY_TRIPLEX, 6, Scalar(0, 0, 0), 8);
+		cout << ";" << endl;
+	}
 	return img;
 }
 
-Mat enter(Mat img) {
+Mat enter(Mat img) {//入室可不可を出力
 	if (out_flag == true) {//入室可能なら
-		PinP_point(img,can,Point2f(CAN_CANT_X,CAN_CANT_Y),Point2f(CAN_CANT_X+400,CAN_CANT_Y+800));
+		img=PinP_tr(img,can,CAN_CANT_X,CAN_CANT_Y);
 	}
 	else if (out_flag==false) {//入室不能なら
-		PinP_point(img, cant, Point2f(CAN_CANT_X, CAN_CANT_Y), Point2f(CAN_CANT_X + 400, CAN_CANT_Y + 800));
+		img=PinP_tr(img, cant, CAN_CANT_X,CAN_CANT_Y);
 	}
 	return img;
 }
 
-void PinP_point(const cv::Mat& srcImg, const cv::Mat& smallImg, const cv::Point2f p0, const cv::Point2f p1)
+Mat PinP_tr(const cv::Mat& srcImg, const cv::Mat& smallImg, const int tx, const int ty)
 {
 	//背景画像の作成
 	cv::Mat dstImg;
 	srcImg.copyTo(dstImg);
 
-	//３組の対応点を作成
-	vector<cv::Point2f> src, dst;
-	src.push_back(cv::Point2f(0, 0));
-	src.push_back(cv::Point2f(smallImg.cols, 0));
-	src.push_back(cv::Point2f(smallImg.cols, smallImg.rows));
-
-	dst.push_back(p0);
-	dst.push_back(cv::Point2f(p1.x, p0.y));
-	dst.push_back(p1);
-
 	//前景画像の変形行列
-	cv::Mat mat = cv::getAffineTransform(src, dst);
+	cv::Mat mat = (cv::Mat_<double>(2, 3) << 1.0, 0.0, tx, 0.0, 1.0, ty);
 
 	//アフィン変換の実行
 	cv::warpAffine(smallImg, dstImg, mat, dstImg.size(), INTER_LINEAR, cv::BORDER_TRANSPARENT);
+	return dstImg;
 }
 
